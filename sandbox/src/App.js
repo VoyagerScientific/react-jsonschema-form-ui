6dc3dd11f836ada1d607b8bc48199276b2e9cdcd
Matt Monihan <mmonihan@gmail.com>
2020-02-28 18:33:43 -0500

    Added bootsrap.

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from 'react-jsonschema-form';
 import bootstrap from 'bootstrap';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { ArrayFieldTemplate } from './components/fields';
 import { CurrencyWidget, ReactDatePickerWidget, ReactSelectWidget } from '../../src/widgets';
 import './App.css';
