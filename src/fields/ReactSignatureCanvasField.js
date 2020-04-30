dec97c0afc7cf322b23c9359c1203e315fee076d
Matt Monihan <mmonihan@gmail.com>
2020-04-30 13:21:11 -0400

    Refactored signature canvas and added a clear button.

--- src/fields/ReactSignatureCanvasField.js
+++ src/fields/ReactSignatureCanvasField.js
@@ -6,7 +6,9 @@ class ReactSignatureCanvasField extends Component{
   constructor(props){
     super(props);
     this.state = {
-      ...props
+      ...props,
+      width: props.uiSchema["ui:options"] && props.uiSchema["ui:options"].width || 400,
+      height: props.uiSchema["ui:options"] && props.uiSchema["ui:options"].height || 150
     }
   }
 
@@ -14,22 +16,49 @@ class ReactSignatureCanvasField extends Component{
     this.sigCanvas.fromDataURL(this.state.formData);
   }
 
+  _clear(e){
+    e.preventDefault();
+
+    const confirmed = confirm("Clear the signature?")
+    if(confirmed){
+      this.sigCanvas.clear();
+      this.state.onChange();
+    }else{
+      return false;
+    }
+  }
+
   render(){
     return (
-      <div>
+      <div style={{position: "relative", width: this.state.width+41, minHeight: this.state.height+40}}>
         <SignatureCanvas
           ref={(ref) => { this.sigCanvas = ref }}
           penColor='black'
           canvasProps={{
-            width: this.props.uiSchema["ui:options"] && this.props.uiSchema["ui:options"].width || 400,
-            height: this.props.uiSchema["ui:options"] && this.props.uiSchema["ui:options"].height || 150,
+            width: this.state.width,
+            height: this.state.height,
             className: 'sigCanvas',
             style: {border: "#ddd 3px dashed", borderRadius: 4}
           }}
           backgroundColor="#fafafa"
           onEnd={(value) => this.state.onChange(this.sigCanvas.toDataURL(value))}
         />
-
+        { this.sigCanvas && !this.sigCanvas.isEmpty() ?
+        <button
+          className="btn btn-secondary d-print-none"
+          style={{
+            position: "absolute",
+            right: 0,
+            top: 0,
+            height: this.state.height+6,
+            fontSize: 16,
+            fontWeight: '700',
+            borderTopLeftRadius: 0,
+            borderBottomLeftRadius: 0,
+            borderWidth: 3
+          }}
+          onClick={this._clear.bind(this)}>&times;</button>
+        : null }
         <div className="sigSalutation">
           <h6 style={{paddingTop: 10, marginTop: 15, borderTop: "#444 1px solid"}}>{this.state.schema.title}</h6>
         </div>
