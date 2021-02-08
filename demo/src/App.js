97c368af5433ff36b0dc411490ea1a124653c906
Kevin de Guzman <aronkev@gmail.com>
2021-02-09 04:59:29 +0800

    Added changes to InputTable Widget (proper usage of button props)

--- demo/src/App.js
+++ demo/src/App.js
@@ -551,9 +551,10 @@ const uiSchema = {
       "rows": ["Critical", "Serious", "Medium", "Small", "Minor"],
       "columns": ["Very Low", "Low", "Medium", "High", "Very High"],
       "values" :[
-      [5, 10, 15, 20, 25],
+      [5, 10, 20, 20, 25],
       [4, 8 , 12, 16, 20],
       [3, 6, 9, 12, 15],
+      [2, 4, 6, 8, 10],
       [1, 2, 3, 4, 5]
       ]
     },
@@ -581,7 +582,11 @@ const formData = {
     "Responsiveness": "Satisfied",
     "Friendliness": "Very Satisfied",
   },
-  input_table_button: "Cleanliness - Satisfied",
+  input_table_button: {
+    row: "Critical",
+    column: "Low",
+    value: 10
+  },
 }
 
 class FormComponent extends Component {
