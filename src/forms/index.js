8f179affb8c9b76f74624ea26e549b5f3059e4f0
Kevin de Guzman <aronkev@gmail.com>
2021-02-16 22:42:37 +0800

    Improved the package.json

--- src/forms/index.js
+++ src/forms/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import React from 'react';
-import Form from "react-jsonschema-form";
+import Form from '@rjsf/core';
 
 class SchemaForm extends React.Component {
   form = React.createRef();
