3bbade3611854b2887f48218980e478bd507d1f0
Kevin de Guzman <aronkev@gmail.com>
2021-05-09 00:17:43 +0800

    Fix in currency to support negative values

--- src/widgets/CurrencyWidget.js
+++ src/widgets/CurrencyWidget.js
@@ -12,7 +12,11 @@ class CurrencyWidget extends Component {
     accounting.settings = {
       currency: {
         symbol: !props.options.symbol ? "$" : props.options.symbol, // default currency symbol is '$'
-        format: !props.options.format ? "%s%v" : props.options.format, // controls output: %s = symbol, %v = value/number
+        format: {
+          pos: !props.options.format ? "%s%v" : props.options.format,
+          neg: !props.options.format ? "%s(%v)" : props.options.format,
+          zero: "%s  -- "
+        }, // controls output: %s = symbol, %v = value/number
         decimal: ".", // decimal point separator
         thousand: ",", // thousands separator
         precision: this.precision, // decimal places
@@ -30,11 +34,40 @@ class CurrencyWidget extends Component {
       value: accounting.formatMoney(props.value / 100),
     };
   }
-  isNumberPressed = (event) => {
-    const keyCode = event.keyCode || event.which;
-    const keyValue = String.fromCharCode(keyCode);
-    //regex that accepts numbers only
-    if (!/^[0-9.]+$/.test(keyValue)) event.preventDefault();
+
+  handleFocus = (event) => {
+    const value = this.unformat(event.target.value);
+
+    if (!parseInt(value) && !_.startsWith("-")) {
+      //if value is 0 the field will reset. and value will set into empty string
+      this.setState({
+        value: "",
+      });
+    } else {
+      this.props.onChange(value);
+      this.setState({
+        value: accounting.unformat(event.target.value),
+      });
+    }
+  }
+
+  unformat = (value) => {
+    return (accounting.unformat(value) * 100).toFixed(this.precision);
+  }
+
+  handleBlur = (event) => {
+    const unformattedValue = this.unformat(event.target.value);
+    this.props.onChange(unformattedValue);
+    this.setState({
+      value: accounting.formatMoney(event.target.value),
+    });
+  }
+
+  handleInputValueChange = (event) => {
+    const isNumberAccepted = new RegExp(/(^[-|\+|0-9][0-9]*)(\.*||\.[0-9]*)$/).test(event.target.value);
+    if (isNumberAccepted || _.isEmpty(event.target.value)) {
+      this.setState({ value: event.target.value });
+    }
   };
 
   render() {
@@ -49,42 +82,9 @@ class CurrencyWidget extends Component {
           style={{ textAlign: "right" }}
           value={this.state.value}
           required={this.state.required}
-          onKeyPress={this.isNumberPressed}
-          onFocus={(event) => {
-            const value = (
-              accounting.unformat(event.target.value) * 100
-            ).toFixed(this.precision);
-
-            if (!parseInt(value)) {
-              //if value is 0 the field will reset. and value will set into empty string
-              this.setState({
-                value: "",
-              });
-            } else {
-              this.state.onChange(value);
-              this.setState({
-                value: accounting.unformat(event.target.value),
-              });
-            }
-          }}
-          onBlur={(event) => {
-            this.state.onChange(
-              (accounting.unformat(event.target.value) * 100).toFixed(
-                this.precision
-              )
-            );
-            this.setState({
-              value: accounting.formatMoney(event.target.value),
-            });
-          }}
-          onChange={(event) => {
-            this.setState({ value: event.target.value }, () =>
-              this.state.onChange(
-                accounting.unformat(this.state.value).toFixed(this.precision) *
-                  100
-              )
-            );
-          }}
+          onChange={this.handleInputValueChange}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
           disabled={this.state.disabled}
           readOnly={this.state.readonly}
         />
