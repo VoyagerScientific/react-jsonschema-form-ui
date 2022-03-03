36f563e2fbf4b9a40a8aace38c1031180a706477
Matt Monihan <mmonihan@voyager.vc>
2022-03-03 14:39:56 -0500

    Updated css on photo item.

--- src/fields/ReactPhotoGalleryField/components/PhotoItem.js
+++ src/fields/ReactPhotoGalleryField/components/PhotoItem.js
@@ -8,13 +8,16 @@ class PhotoItem extends React.Component {
         <div onClick={this.props.onDeleteButtonClick}>
           <FontAwesome
             style={{
-              color: "white",
+              color: "#e30000",
+              backgroundColor: 'white',
               fontSize: "20px",
               position: "absolute",
               right: "5px",
               top: "5px",
+              height: "19px",
+              borderRadius: "2px"
             }}
-            className={"d-print-none"}
+            className={"d-print-none remove-photo-item-btn"}
             name="window-close"
           />
         </div>
